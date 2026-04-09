b5ba965d41e853b4f462b319bbb9a5fe5f445bc5
Michael Todd <michael@pocketmaps.com>
2026-04-09 00:42:31 -0400

    Remove Suspense wrapper from ExplorePage to simplify rendering of HomeV2ExploreContent, enhancing performance and reducing complexity.

--- app/(prism)/explore/page.tsx
+++ app/(prism)/explore/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react"
-import { HomeV2ExploreContent, HomeV2Fallback } from "@/components/pride-home-client"
+import { HomeV2ExploreContent } from "@/components/pride-home-client"
 
 export default function ExplorePage() {
-  return (
-    <Suspense fallback={<HomeV2Fallback />}>
-      <HomeV2ExploreContent />
-    </Suspense>
-  )
+  return <HomeV2ExploreContent />
 }
